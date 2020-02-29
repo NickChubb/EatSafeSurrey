afe78e4a75bf1de60f7f824aa20da79be4ae992e
yukun wang <wyk1999315@gmail.com>
2020-02-29 13:51:51 -0800

    delete the welcome screen's toolbar

--- app/src/main/java/com/example/restaurantinsurrey/MainActivity.java
+++ app/src/main/java/com/example/restaurantinsurrey/MainActivity.java
@@ -27,14 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
 
         // animation of the splash screen
         setIconAnim();
@@ -51,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
         };
         handler.postDelayed(runnable,3000);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
