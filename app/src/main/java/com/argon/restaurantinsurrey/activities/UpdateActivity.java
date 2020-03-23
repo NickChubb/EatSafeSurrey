885f2c89d477306855b37df8af3b3f440559ef78
Nick Chubb <Nick.r.chubb@gmail.com>
2020-03-23 16:57:54 -0700

    add cancel button, other UI

--- app/src/main/java/com/argon/restaurantinsurrey/activities/UpdateActivity.java
+++ app/src/main/java/com/argon/restaurantinsurrey/activities/UpdateActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.argon.restaurantinsurrey.R;
 import com.argon.restaurantinsurrey.model.UpdateManager;
@@ -34,6 +36,14 @@ public class UpdateActivity extends AppCompatActivity {
 
         updateManager.updateData(availableUpdates);
 
+
+        Button btnCancel = findViewById(R.id.buttonCancelUpdate);
+        btnCancel.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
 
     }
